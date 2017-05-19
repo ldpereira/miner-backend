d8f25796b62493ea7ce819c5458de928419d7032
Lucas Dalcol Pereira <ldpereira@icloud.com>
2017-05-19 02:08:34 -0300

    Adjust

--- MinerWS/src/main/java/com/miner/minerws/WebService.java
+++ MinerWS/src/main/java/com/miner/minerws/WebService.java
@@ -6,20 +6,11 @@
 package com.miner.minerws;
 
 import com.miner.minerws.miners.minerj48.MinerJ48;
-import com.miner.minerws.model.Database;
-import com.miner.minerws.model.Group;
-import com.miner.minerws.model.Message;
 import com.miner.minerws.model.Node;
-import com.miner.minerws.model.ReportMensagem;
-import com.miner.minerws.model.ReportUserGroup;
-import com.miner.minerws.model.User;
-import java.util.ArrayList;
-import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -31,10 +22,10 @@ import javax.ws.rs.core.Response;
 public class WebService {
 
     @GET
-    @Path("/minerJ48/{texto}")
+    @Path("/minerJ48/{file}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response minerJ48(@PathParam("texto") String texto) {
-        Node node = new MinerJ48().minerJ48();
+    public Response minerJ48(@PathParam("file") String file) {
+        Node node = new MinerJ48().minerJ48(file);
         return Response.status(200)
                     .header("Access-Control-Allow-Origin", "*")
                     .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
@@ -42,239 +33,19 @@ public class WebService {
                     .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
                     .header("Access-Control-Max-Age", "1209600")
                     .entity(node).build();
-        /*} else {
-            return Response.status(400)
+    }
+    
+    @GET
+    @Path("/minerJ48/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response minerJ48() {
+        Node node = new MinerJ48().minerJ48(null);
+        return Response.status(200)
                     .header("Access-Control-Allow-Origin", "*")
                     .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
                     .header("Access-Control-Allow-Credentials", "true")
                     .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
                     .header("Access-Control-Max-Age", "1209600")
-                    .entity("Usuário ou senha inválidos!").build();
-        }*/
-    }
-
-    @GET
-    @Path("/login/{username}/{password}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response login(@PathParam("username") String username, @PathParam("password") String password) {
-        User user = Database.getInstance().getUser(username, password);
-        if (user != null) {
-            return Response.status(200)
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .entity(user).build();
-        } else {
-            return Response.status(400)
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .entity("Usuário ou senha inválidos!").build();
-        }
-    }
-
-    @GET
-    @Path("/postUser/{username}/{password}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response singup(@PathParam("username") String username, @PathParam("password") String password) {
-        try {
-            User user = Database.getInstance().createUser(username, password, "");
-            return Response.status(200)
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .entity(user).build();
-        } catch (IllegalArgumentException e) {
-            return Response.status(400)
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .entity(e.getMessage()).build();
-        }
-    }
-
-    @GET
-    @Path("/getUsers")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getUsers() {
-        List<User> list = new ArrayList<User>(Database.getInstance().getUsers().values());
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(new GenericEntity<List<User>>(list) {
-                }).build();
-    }
-
-    @GET
-    @Path("/getGroups")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getGroups() {
-        List<Group> list = new ArrayList<Group>(Database.getInstance().getGroups().values());
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(new GenericEntity<List<Group>>(list) {
-                }).build();
-    }
-
-    @GET
-    @Path("/getMyGroups/{username}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getGroups(@PathParam("username") String username) {
-        List<Group> list = new ArrayList<Group>(Database.getInstance().getGroupsUser(username).values());
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(new GenericEntity<List<Group>>(list) {
-                }).build();
-    }
-
-    @GET
-    @Path("/postGroup/{name}/{username}/{description}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response postGroup(@PathParam("name") String title, @PathParam("username") String username, @PathParam("description") String description) {
-        int idGroup = Database.getInstance().addGroup(title, username, description);
-
-        if (idGroup > 0) {
-            return Response.status(200)
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .entity(Database.getInstance().getGroups().get(idGroup)).build();
-        }
-        return Response.status(400)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity("Não foi possível criar o grupo").build();
-    }
-
-    @GET
-    @Path("/deleteGroup/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteGroup(@PathParam("id") String id) {
-        if (Database.getInstance().getGroups().remove(Integer.valueOf(id)) != null) {
-
-            return Response.status(200)
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .entity("Grupo excluído com sucesso").build();
-        } else {
-            return Response.status(400)
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Credentials", "true")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Max-Age", "1209600")
-                    .entity("Grupo inexistente").build();
-        }
-    }
-
-    /**
-     * Recebe as mensagens.
-     *
-     * @param group ID do grupo.
-     * @param username Nome do usuário.
-     * @param message Conteúdo da mensagem.
-     * @return boolean indicando sucesso no envio da mensagem.
-     */
-    @GET
-    @Path("/sendMessage/{group}/{username}/{message}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response sendMessage(@PathParam("group") String group, @PathParam("username") String username, @PathParam("message") String message) {
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(Database.getInstance().sendMessage(Integer.valueOf(group), username, message)).build();
-    }
-
-    /**
-     * Obtém as mensagens no banco de dados.
-     *
-     * @param group ID do grupo.
-     * @return Mensagens do grupo.
-     */
-    @GET
-    @Path("/getMessages/{group}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getMessages(@PathParam("group") String group) {
-        List<Message> list = Database.getInstance().getMessages(Integer.parseInt(group));
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(new GenericEntity<List<Message>>(list) {
-                }).build();
-    }
-
-    @GET
-    @Path("/getUsuariosGrupos")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getUsuariosGrupos() {
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(new GenericEntity<List<ReportUserGroup>>(Database.getInstance().getUsuariosGrupos()) {
-                }).build();
-    }
-
-    @GET
-    @Path("/getMensagensGrupos")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getMensagensGrupos() {
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(new GenericEntity<List<ReportMensagem>>(Database.getInstance().getMensagensGrupos()) {
-                }).build();
-    }
-
-    @GET
-    @Path("/getMensagensUsuarios")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getMensagensUsuarios() {
-        return Response.status(200)
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                .header("Access-Control-Max-Age", "1209600")
-                .entity(new GenericEntity<List<ReportMensagem>>(Database.getInstance().getMensagensUsuarios()) {
-                }).build();
+                    .entity(node).build();
     }
 }
--- MinerWS/src/main/java/com/miner/minerws/miners/minerj48/MinerJ48.java
+++ MinerWS/src/main/java/com/miner/minerws/miners/minerj48/MinerJ48.java
@@ -6,8 +6,8 @@
 package com.miner.minerws.miners.minerj48;
 
 import com.miner.minerws.model.Node;
-import java.io.File;
 import java.io.FileInputStream;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import weka.classifiers.trees.J48;
 import weka.core.DenseInstance;
@@ -22,8 +22,14 @@ import weka.core.converters.ConverterUtils;
  */
 public class MinerJ48 {
 
-    public Node minerJ48() {
-        String retornoWeka = executeWekaJ48();
+    public Node minerJ48(String file) {
+        String retornoWeka;
+
+        if (file == null || file.trim().isEmpty()) {
+            retornoWeka = executeWekaJ48();
+        } else {
+            retornoWeka = executeWekaJ48("C:/temp/" + file);
+        }
 
         System.out.println(retornoWeka);
         String[] list = retornoWeka.split("\n");
@@ -40,9 +46,9 @@ public class MinerJ48 {
                     String title = text.substring(text.indexOf("\"") + 1, text.lastIndexOf("\""));
 
                     if (nodes.containsKey(key)) {
-                        nodes.get(key).setText(title);
+                        nodes.get(key).setText(formatText(title));
                     } else {
-                        Node node = new Node(key, title);
+                        Node node = new Node(key, formatText(title));
                         nodes.put(node.getKey(), node);
 
                         if (root == null) {
@@ -52,22 +58,22 @@ public class MinerJ48 {
                 } else {
                     String[] nodeString = item.split("->");
                     Node nodeParent = nodes.get(nodeString[0].trim());
-                    
+
                     String[] restString = nodeString[1].split("\\[");
                     String key = restString[0].trim();
                     String text = restString[1];
                     String title = text.substring(text.indexOf("\"") + 1, text.lastIndexOf("\""));
-                    
+
                     Node node;
                     if (nodes.containsKey(key)) {
                         node = nodes.get(key);
                     } else {
-                        node = new Node(key, title);
+                        node = new Node(key, formatTitle(title));
                     }
-                    
-                    node.setLabel(title);
+
+                    node.setLabel(formatTitle(title));
                     nodeParent.addNode(node);
-                    
+
                     nodes.put(node.getKey(), node);
                 }
             }
@@ -109,4 +115,178 @@ public class MinerJ48 {
         }
         return "Can't process the file arff";
     }
+
+    public static String executeWekaJ48(String file) {
+        try {
+            ConverterUtils.DataSource ds = new ConverterUtils.DataSource(new FileInputStream(file));
+            Instances ins = ds.getDataSet();
+            ins.setClassIndex(8);
+
+            J48 j48 = new J48();
+            //cria o classificador
+            j48.buildClassifier(ins);
+
+            Instance novo = new DenseInstance(8);
+            //vincula o arquivo e os dados
+            novo.setDataset(ins);
+            novo.setValue(0, "SantanaShopping");
+            novo.setValue(1, "quarta-feira");
+            novo.setValue(2, "Fevereiro");
+            novo.setValue(3, "Tarde");
+            novo.setValue(4, "FluxoModerado");
+            novo.setValue(5, "VitrineMedio");
+            novo.setValue(6, "Morno");
+            novo.setValue(7, "PoucaChuva");
+
+            //gerar a previsao do campo gasta_muito
+            double retorno = j48.classifyInstance(novo);
+            System.out.println("retorno: " + retorno);
+            String graph = ((Drawable) j48).graph();
+            System.out.println("#########\n" + j48.numElements() + "\n" + j48.measureTreeSize() + "\n" + graph);
+
+            return graph;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Can't process the file arff";
+    }
+
+    public static String formatTitle(String text) {
+        String textChanged;
+        if ("= SemFluxo".equals(text)) {
+            textChanged = "= Sem fluxo (0 clientes)";
+        } else if ("= FluxoBaixo".equals(text)) {
+            textChanged = "= Fluxo baixo (1 a 50 clientes)";
+        } else if ("= FluxoModerado".equals(text)) {
+            textChanged = "= Fluxo moderado (51 a 100 clientes)";
+        } else if ("= FluxoAlto".equals(text)) {
+            textChanged = "= Fluxo alto (101 a 200 clientes)";
+        } else if ("= VitrineNenhumMovimento".equals(text)) {
+            textChanged = "= Sem movimento (0 visitantes)";
+        } else if ("= VitrineBaixo".equals(text)) {
+            textChanged = "= Movimento baixo (1 a 90 visitantes)";
+        } else if ("= VitrineMedio".equals(text)) {
+            textChanged = "= Movimento médio (91 a 300 visitantes)";
+        } else if ("= VitrineAlto".equals(text)) {
+            textChanged = "= Movimento alto (301 a 743 visitantes)";
+        } else if ("= Manha".equals(text)) {
+            textChanged = "= Matutino (10h - 12h)";
+        } else if ("= Tarde".equals(text)) {
+            textChanged = "= Vespertino (13h-18h)";
+        } else if ("= Noite".equals(text)) {
+            textChanged = "= Noturno (19h-22h)";
+        } else if ("= Frio".equals(text)) {
+            textChanged = "= Frio (18ºC a 21ºC)";
+        } else if ("= Morno".equals(text)) {
+            textChanged = "= Morno (21,1ºC a 24ºC)";
+        } else if ("= Quente".equals(text)) {
+            textChanged = "= Quente (24,1ºC a 27ºC)";
+        } else if ("= MuitoQuente".equals(text)) {
+            textChanged = "= Muito quente (27.1ºC a 34ºC)";
+        } else if ("= SantanaShopping".equals(text)) {
+            textChanged = "= Shopping Santana";
+        } else if ("= PinheirosShopping".equals(text)) {
+            textChanged = "= Shopping Pinheiros";
+        } else if ("= SemChuva".equals(text)) {
+            textChanged = "= Ensolarado (0mm)";
+        } else if ("= PoucaChuva".equals(text)) {
+            textChanged = "= Pouca chuva (0mm a 1mm)";
+        } else if ("= ChuvaLeve".equals(text)) {
+            textChanged = "= Chuva leve (2mm a 4,6mm)";
+        } else if ("= ChuvaModerada".equals(text)) {
+            textChanged = "= Chuva moderada (4,7mm a 8,2mm)";
+        } else if ("= ChuvaForte".equals(text)) {
+            textChanged = "= Chuva forte (8,2mm a 26,5mm)";
+        } else if ("= Tempestade".equals(text)) {
+            textChanged = "= Tempestade (26,6mm a 46,6mm)";
+        } else if ("= Marco".equals(text)) {
+            textChanged = "= Março";
+        } else {
+            textChanged = text;
+        }
+
+        return textChanged;
+    }
+
+    public static String formatText(String title) {
+        String titleChanged;
+
+        if ("Loja".equalsIgnoreCase(title)) {
+            titleChanged = "Se loja:";
+        } else if ("DiaDaSemana".equalsIgnoreCase(title)) {
+            titleChanged = "Se dia da semana:";
+        } else if ("Mes".equalsIgnoreCase(title)) {
+            titleChanged = "Se mês:";
+        } else if ("PeriodoDoDia".equalsIgnoreCase(title)) {
+            titleChanged = "Se período:";
+        } else if ("Fluxo".equalsIgnoreCase(title)) {
+            titleChanged = "Se fluxo:";
+        } else if ("FluxoDaVitrine".equalsIgnoreCase(title)) {
+            titleChanged = "Se fluxo da vitrine:";
+        } else if ("Temperatura".equalsIgnoreCase(title)) {
+            titleChanged = "Se temperatura:";
+        } else if ("PrevisaoDoTempo".equalsIgnoreCase(title)) {
+            titleChanged = "Se previsão do tempo:";
+        } else if ("Atendimentos".equalsIgnoreCase(title)) {
+            titleChanged = "Então, possível quantidade de atendimento:";
+        } else if (title.contains("Atendimento")) {
+            if (title.contains("/")) {
+                String[] valores = title.substring(title.indexOf("(") + 1, title.indexOf(")")).split("/");
+                double vlPositivo = Double.valueOf(valores[0]);
+                double vlNegativo = Double.valueOf(valores[1]);
+
+                Double acertividade = (100 - ((vlNegativo * 100) / vlPositivo));
+                DecimalFormat decimalFormat = new DecimalFormat("#.00");
+                String acertividadeString = decimalFormat.format(acertividade) + "%";
+
+                if (title.startsWith("SemAtendimento")) {
+
+                    decimalFormat = new DecimalFormat("#.0");
+
+                    titleChanged = String.format("Extimativa de atendimento: sem atendimento, %s de acertividade baseado em "
+                            + "registros na base de conhecimento.",
+                            acertividadeString, decimalFormat.format(vlPositivo + vlNegativo));
+                } else {
+                    String vlInferior = title.substring(0, title.indexOf("-"));
+                    String vlSuperior = title.substring(title.indexOf("-")+1, title.indexOf("Atend"));
+
+                    titleChanged = String.format("Extimativa de atendimento: de %s à %s, %s de acertividade baseado em "
+                            + "%s registros na base de conhecimento.",
+                            vlInferior, vlSuperior, acertividadeString, decimalFormat.format((vlPositivo + vlNegativo)));
+                }
+            } else {
+                String valor = title.substring(title.indexOf("(") + 1, title.indexOf(")"));
+                double vlPositivo = Double.valueOf(valor);
+
+                DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
+                if (title.startsWith("SemAtendimento")) {
+                    decimalFormat = new DecimalFormat("#.0");
+
+                    titleChanged = String.format("Extimativa de atendimento: sem atendimento, %s de acertividade baseado em "
+                            + "%s registros na base de conhecimento.",
+                            "100%", decimalFormat.format(vlPositivo));
+                } else if (title.startsWith("19Atendimentos")) {
+                    decimalFormat = new DecimalFormat("#.0");
+
+                    titleChanged = String.format("Extimativa de atendimento: 19 atendimentos, %s de acertividade baseado em "
+                            + "%s registros na base de conhecimento.",
+                            "100%", decimalFormat.format(vlPositivo));
+                } else {
+
+                    String vlInferior = title.substring(0, title.indexOf("-"));
+                    String vlSuperior = title.substring(title.indexOf("-")+1, title.indexOf("Atend"));
+
+                    titleChanged = "Extimativa de atendimento: de " + vlInferior + " à " + vlSuperior
+                            + ", 100% de acertividade baseado em "
+                            + vlPositivo + " registros na base de conhecimento.";
+                }
+            }
+        } else {
+            titleChanged = title;
+        }
+
+        return titleChanged;
+    }
+
 }
